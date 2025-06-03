b763daeba5d080ecb977db4194ea21ab9f1bb432
pablorenato1 <pablorenato18@gmail.com>
2025-06-02 23:13:36 -0300

    Create staging model and tests for customers data

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,15 @@
+with 
+    source as (
+        select * 
+        from {{ ref('raw_customers') }}
+    )
+
+    , renamed as (
+        select 
+            id as customer_id
+            , name as customer_name
+        from source
+    )
+
+select * 
+from renamed
