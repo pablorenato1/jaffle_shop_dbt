8ee8eadbaf5be4ac6f873430e0847d1266cee651
pablorenato1 <pablorenato18@gmail.com>
2025-06-02 23:13:05 -0300

    Create staging model and tests for ordered Items data

---
+++ models/staging/stg_order_items.sql
@@ -0,0 +1,16 @@
+with 
+    source as (
+        select * 
+        from {{ ref("raw_items") }}
+    )
+
+    , renamed as (
+        select
+            id as order_item_id
+            , order_id 
+            , sku as product_id
+        from source
+    )
+
+select * 
+from renamed
